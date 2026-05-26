24ec367f3c6ce67f269090e19cc5b928d3891e22
Linus Shoravi <linuss@spotify.com>
2026-05-26 07:05:22 +0200

    feat: add %perform-operation-block async bridge for fibers CML suspension

--- src/lib.rs
+++ src/lib.rs
@@ -45,6 +45,8 @@ impl<L, R> Either<L, R> {
 #[cfg(feature = "tokio")]
 pub mod atomic;
 #[cfg(feature = "tokio")]
+pub mod fibers;
+#[cfg(feature = "tokio")]
 pub mod futures;
 
 // Require tokio (for now) if the async feature is enabled
